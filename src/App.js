60bd31f0554f182a14f719dde038b21ee6c24786
Beta-23 <alfred.dominguez@gmail.com>
2020-07-13 13:25:09 -0700

    test the on props callbacks for video player

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
-
+import ReactPlayer from 'react-player';
 import './App.css';
 
 function App () {
-	return <div className='App' />;
+	return (
+		<div className='App'>
+			<ReactPlayer
+				width='480px'
+				height='240px'
+				controls
+				url='https://youtu.be/TQUy8ENesGY'
+				onReady={() => console.log('onReady callback')}
+			/>
+		</div>
+	);
 }
 
 export default App;
