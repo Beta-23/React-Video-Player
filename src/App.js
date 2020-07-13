c9c75b9df23228358469ebd2b89dd7647af69499
Beta-23 <alfred.dominguez@gmail.com>
2020-07-13 14:20:19 -0700

    Convert to separate video player component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import ReactPlayer from 'react-player';
+import ResponsivePlayer from './components/ResponsivePlayer';
 import './App.css';
 
 function App () {
 	return (
 		<div className='App'>
-			<ReactPlayer
-				width='480px'
-				height='240px'
-				controls
-				url='https://www.twitch.tv/videos/674638399'
-				onReady={() => console.log('onReady callback')}
-				onStart={() => console.log('onStart callback')}
-				onPause={() => console.log('onPause callback')}
-				onEnded={() => console.log('onEnded callback')}
-				onError={() => console.log('onError callback')}
-			/>
+			<ResponsivePlayer />
 		</div>
 	);
 }
---
+++ src/components/ResponsivePlayer.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactPlayer from 'react-player';
+
+// Stateless functional component
+const ResponsivePlayer = () => (
+	<div className='player-wrapper'>
+		<ReactPlayer
+			className='react-player'
+			controls
+			url='https://www.twitch.tv/videos/674638399'
+			onReady={() => console.log('onReady callback')}
+			onStart={() => console.log('onStart callback')}
+			onPause={() => console.log('onPause callback')}
+			onEnded={() => console.log('onEnded callback')}
+			onError={() => console.log('onError callback')}
+		/>
+	</div>
+);
+
+export default ResponsivePlayer;
